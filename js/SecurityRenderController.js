6bd600c75e2295c9f817b6c0f2eb85e2a0d37554
Matt Hoyt <mrhoyt4@yahoo.com>
2011-03-28 23:04:47 -0500

    Initial load of a security component for extjs.

---
+++ js/SecurityRenderController.js
@@ -0,0 +1,117 @@
+Ext.ns('Ext.ux');
+
+/**
+ * Used to render gui components.
+ */
+Ext.ux.SecurityRenderController = function() {
+  this.roles = undefined;
+};
+
+Ext.ux.SecurityRenderController.prototype = {
+  /**
+   * Used to render classes based on their on there security.
+   *
+   * If no security is given the map it is assumed that it doesn't require
+   * any specific security level to view this component.
+   */
+  render : function() {
+    var renderObj = undefined;
+    if (arguments.length > 1) {
+      renderObj = this.processObjArray(arguments);
+    } else {
+      renderObj = this.processSingleArgument(arguments[0]);
+    }
+    return renderObj;
+  },
+
+  /**
+   * used to render a top level component. (It doesn't return an array).
+   * 
+   * @param topObj The toplevel obj.
+   */
+  renderTop : function(topObj) {
+    return this.render(topObj)[0];
+  },
+
+  //private
+  processSingleArgument : function(argument) {
+    var renderObj = undefined;
+    var argument = arguments[0];
+    if (argument instanceof Array) {
+      renderObj = this.processObjArray(argument);
+    } else {
+      renderObj = this.processObj(argument);
+    }
+    return renderObj;
+  },
+
+  processObjArray : function(objArray) {
+    var renderObj = [];
+    for (var i = 0; i < objArray.length; i ++) {
+      var current = objArray[i];
+      if (this.isUserAbleToView(current)) {
+        renderObj.push(current);
+      }
+    }
+    return renderObj;
+  },
+
+  processObj : function(obj) {
+    var renderObj = []
+    if (this.isUserAbleToView(obj)) {
+      renderObj = [obj];
+    }
+    return renderObj;
+  },
+
+  //private
+  isUserAbleToView : function(obj) {
+    var roles = obj['roles'];
+    var valid = false;
+    if (roles) {
+      valid = this.checkRoles(roles);
+    } else {
+      valid = true;
+    }
+    this.processChildren(obj, valid);
+    return valid;
+  },
+
+  //private
+  processChildren : function(obj, valid) {
+    if (valid) {
+      if (obj.items) {
+        obj.items= this.render(obj.items);
+      }
+    }
+  },
+
+  checkRoles : function(roles) {
+    var valid = false;
+    if (roles instanceof Array) {
+      for (var i = 0; i < roles.length; i ++) {
+        if (this.hasRole(roles[i])) {
+          valid = true;
+        }
+      }
+    } else {
+      valid = this.hasRole(roles);
+    }
+    return valid;
+  },
+
+  /**
+   * Used to check to see if a user has a role.
+   * @param role The role to see if the user has.
+   */
+  hasRole : function(role) {
+    for (var i = 0; i < this.roles.length; i ++) {
+      if (this.roles[i] == role) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+Ext.ux.secure = new Ext.ux.SecurityRenderController();
---
+++ testing/spec/controllers/SecurityRenderControllerTest.js
@@ -0,0 +1,72 @@
+describe('Security Render Controller Tests', function() {
+
+  beforeEach(function() {
+    Ext.ux.secure.roles = ['admin', 'user'];
+  });
+
+  it('matching roles test array', function() {
+    Ext.ux.secure.roles = ["admin", "user"];
+    var testObject = [
+      {id: "obj1", roles : "user"},
+      {id: "obj2", roles : "bob"}
+    ];
+    Ext.ux.secure.render(testObject);
+  });
+
+  it('Matching roles test single', function() {
+    Ext.ux.secure.roles = ["admin"];
+    var testObject = {id: "obj1", roles : 'admin'};
+    expect(Ext.ux.secure.render(testObject)).toEqual([testObject]);
+
+    testObject = {id: "obj1", roles: 'user'};
+    expect(Ext.ux.secure.render(testObject)).toEqual([]);
+  });
+
+  it('Matching roles test multiple', function() {
+    Ext.ux.secure.roles = ["admin", "user"];
+    var validObj = [
+      {id: "obj1", roles : 'admin'},
+      {id: "obj2", roles : 'user'}
+    ];
+    expect(Ext.ux.secure.render({id: "obj1", roles : 'admin'}, {id: "obj2", roles : 'user'})).
+      toEqual(validObj);
+  });
+
+  it('Testing has roles', function() {
+    expect(Ext.ux.secure.hasRole('admin')).toEqual(true);
+
+    Ext.ux.secure.roles = ['admin'];
+    expect(Ext.ux.secure.hasRole('user')).toEqual(false)
+  });
+
+  it('Test when obj has no roles', function() {
+    Ext.ux.secure.roles = ['admin'];
+    var testObject = {id : 'hello'};
+    var validObject = {id : 'hello'};
+    expect(Ext.ux.secure.render(testObject)).toEqual([validObject]);
+  })
+
+  it('Test nested items', function() {
+    var items = [
+      {id: "obj1", roles : 'admin'},
+      {id: "obj2", roles : 'user',
+        items : [{id: "obj1", roles : 'admin'}]}
+    ];
+    var testObject = {id : 'hello', items : items};
+    var validObject = {id : 'hello', items : items};
+    expect(Ext.ux.secure.renderTop(testObject)).toEqual(validObject);
+  });
+
+  it('Test nested items invalid', function() {
+    var items = [
+      {id: "obj1", roles : 'admin'},
+      {id: "obj2", roles : 'users',
+        items : [{id: "obj1", roles : 'other'}]}
+    ];
+    var testObject = {id : 'hello', items : items};
+
+    var valid = {id : 'hello', items : [{id: "obj1", roles : 'admin'}]};
+
+    expect(Ext.ux.secure.renderTop(testObject)).toEqual(valid);
+  });
+});
